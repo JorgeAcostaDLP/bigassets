ff41867256975eb47292e3283ac856bd957bfd37
Jorge Acosta <jh.acostadelapena@gmail.com>
2020-02-20 14:14:27 -0500

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-for (let i = 1; i < 800; i++) {
-  fetch(`https://fizal.me/pokeapi/api/v2/id/${i}.json`)
-    .then(function(r){
-      return r.json();
-    })
-    .then(function(data) {
-      document.getElementById("images").innerHTML += `
-        <img class="target" src="${data.sprites.front_default}" alt="sprite">
+async function getEmAll() {
+  for (let i = 1; i < 800; i++) {
+    await fetch(`https://fizal.me/pokeapi/api/v2/id/${i}.json`)
+      .then(function(r) {
+        return r.json();
+      })
+      .then(function(data) {
+        document.getElementById('images').innerHTML += `
+        <img class="target" src="${data.sprites.front_default}"  loading="lazy" alt="sprite">
       `;
-    });
+      });
+  }
 }
+
+getEmAll();
